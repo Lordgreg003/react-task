ac1f69fa2061302c92db154aca9c2d879eb98f2a
lordgreg003 <chideranwokoye555@gmail.com>
2023-09-16 19:04:16 +0100

    react-task

---
+++ src/Greg.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+const Greg = () => {
+  return (
+    <div>
+      <span>
+        <button className='bg-[#69b99d] w-[340px] h-10 rounded-md'>
+          Login
+        </button>
+      </span>
+    </div>
+  );
+};
+
+export default Greg;
---
+++ src/images/index.js
@@ -0,0 +1,3 @@
+export { default as image1 } from './image1.svg';
+export { default as toggle1 } from './toggle.svg';
+export { default as image2 } from './image2.png';
